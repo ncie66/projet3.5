ea53d8734eca4e5aa33fd34669fd5b72fc10b3ab
FATDANOUNE 66 <ym66300@gmail.com>
2018-02-09 16:58:50 +0100

    vrai fin des cat

--- index.php
+++ index.php
@@ -3,33 +3,45 @@
 <div class="containeur">
 
   <div class="category-auto auto">
+
     <div class="sub-category">
       <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
+      <div class="s-cat mecanique"></div> <div class="s-cat mecanique"></div>
       <div class="s-cat mecanique"></div>
       <div class="s-cat mecanique"> 
         <div class="s-cat-container">
           sdkqsj
         </div>
       </div>
-      <div class="s-cat "></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
-      <div class="s-cat"></div>
+      <div class="s-cat mecanique "></div>
+
     </div>
   </div>
 
   <div class="category-auto astro">
-    <div class="sub-category">
-      <div class="s-cat">Sport 7</div>
-      <div class="s-cat">Sport 8</div>
-      <div class="s-cat">Sport 9</div>
+  <div class="sub-category">
+  <div class="s-cat mecanique"></div>
+  <div class="s-cat mecanique"></div>
+  <div class="s-cat mecanique"> 
+    <div class="s-cat-container">
+      sdkqsj
     </div>
   </div>
+  <div class="s-cat mecanique "></div>
+
+</div>
+</div>
 
   <div class="category-auto"></div>
   <div class="category-auto"></div>
@@ -42,7 +54,37 @@
   <div class="category-auto"></div>
   <div class="category-auto"></div>
   <div class="category-auto"></div>
-
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>  
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
+  <div class="category-auto"></div>
 </div>
 
 <?php require 'php/footer.php'; ?>
